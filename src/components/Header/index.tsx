7abfc006ba6e8d7efb402b6dbc0df47295fdb500
Rodrigo Ramos <digojramos@gmail.com>
2023-03-03 09:29:27 -0300

    Project create, clean

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import './styles.css';
+
+export default function Header() {
+    return(
+        <header>
+            <nav>
+                <div>
+                    <h1>DSFilter</h1>
+                </div>
+                <div>
+                    <p>6 produto(s)</p>
+                </div>
+            </nav>
+        </header>
+    )
+}
