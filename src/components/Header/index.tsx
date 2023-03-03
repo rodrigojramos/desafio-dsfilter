b4507ec48da71162e7131e3115a320faa411192d
Rodrigo Ramos <digojramos@gmail.com>
2023-03-03 10:08:47 -0300

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ import './styles.css';
 
 export default function Header() {
     return(
-        <header>
-            <nav>
+        <header className="dsf-header-container dsf-mb20">
+            <nav className="dsf-container dsf-navbar">
                 <div>
                     <h1>DSFilter</h1>
                 </div>
